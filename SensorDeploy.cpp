19432a4bd83f61c8686b4d801ea22a77239a0568
Murray Lowery-Simpson <murraylowerysimpson@gmail.com>
2019-06-05 12:06:04 -0300

    Added SensorDeploy class
    Added SensorDeploy class for deploying / retracting sensors to and from the water. It sends pulses to a servo motor to accomplish this.

---
+++ SensorDeploy.cpp
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include "SensorDeploy.h"
+#include "filedata.h"
+
+
+/**
+ * @brief Construct a new SensorDeploy object for controlling the servo motor that is used to deploy and retract sensors into and from the surrounding water
+ * 
+ */
+SensorDeploy::SensorDeploy(char *rootFolder, bool bInitializeWiringPi) {//SensorDeploy constructor
+    m_rootFolder = nullptr;
+    m_bDeployed = false;
+	if (rootFolder!=nullptr) {
+        m_rootFolder = new char[strlen(rootFolder)+1];
+        strcpy(m_rootFolder,rootFolder);
+        GetSettings();
+    }
+    if (bInitializeWiringPi) {
+        OneTimeSetup();
+    }
+}
+
+/**
+ * @brief destructor
+ * 
+ */
+SensorDeploy::~SensorDeploy() {//destructor
+	if (m_rootFolder!=nullptr) {
+        delete m_rootFolder;
+        m_rootFolder = nullptr;
+    }
+}
+
+
+/**
+ * @brief static method that should be called prior to creating and using a Rudder object
+ * 
+ */
+void SensorDeploy::OneTimeSetup() {//setup procedure to be performed once per power-cycle of the deployment servo motor
+	wiringPiSetupGpio();//need to call this before doing anything with GPIO
+    pinMode(DEPLOYMENT_PULSE_PIN,OUTPUT);
+}
+
+/**
+ * @brief get sensor arm deployment status (if available) from the prefs.txt file found in the root program folder
+ * 
+ */
+void SensorDeploy::GetSettings() {
+    if (m_rootFolder==nullptr) return;
+    char prefsFilename[FILENAME_MAX];
+    sprintf(prefsFilename,"%s/prefs.txt",m_rootFolder);
+    filedata prefsFile(prefsFilename);
+    if (prefsFile.FindString("[sensors]","deployed")<=0) {
+        return;//deployment state is not available in file
+    } 
+    int nDeployed = prefsFile.getInteger("[sensors]","deployed");
+    if (nDeployed>0) {
+        m_bDeployed = true;
+    }
+    else {
+        m_bDeployed = false;
+    }
+}
+
+void SensorDeploy::SaveSettings() {
+    if (m_rootFolder==nullptr) return;
+    char prefsFilename[FILENAME_MAX];
+    sprintf(prefsFilename,"%s/prefs.txt",m_rootFolder);
+    filedata prefsFile(prefsFilename);    
+    prefsFile.writeData("[sensors]","deployed",(int)m_bDeployed);
+}
+
+void SensorDeploy::Deploy() {//move the servo motor to deploy the sensors into the water
+    if (m_bDeployed) {
+        //already deployed, just make sure that servo is at the max angle
+        Pulse(MAX_SD_PULSE_TIME, 5000);//start thread to send pulses corresponding to maximum angle for 5 seconds
+    }
+    else {//gradually move through increasing angles to swing the sensor armature into the water
+        for (int i=MIN_SD_PULSE_TIME;i<=MAX_SD_PULSE_TIME;i+=100) {
+            Pulse(i,100);//start thread to send pulses corresponding to a specific angle for 100 ms
+        }
+    }
+    m_bDeployed = true;
+    //save setting to prefs.txt file
+    SaveSettings();
+}
+
+void SensorDeploy::Retract() {//move the servo motor to retract the sensor from the water back onto the boat deck
+    if (!m_bDeployed) {
+        //already retracted, just make sure that servo is at the min angle
+        Pulse(MIN_SD_PULSE_TIME, 5000);//start thread to send pulses corresponding to minimum angle for 5 seconds
+    }
+    else {//gradually move through decreasing angles to swing the sensor armature out of the water and back onto the boat deck
+        for (int i=MAX_SD_PULSE_TIME;i>=MIN_SD_PULSE_TIME;i-=100) {
+            Pulse(i,100);//start thread to send pulses corresponding to a specific angle for 100 ms
+        }
+    }
+    m_bDeployed = false;
+    //save setting to prefs.txt file
+    SaveSettings();
+}
+
+void SensorDeploy::Pulse(int nPulseTimeMicroseconds, int nDurationMilliseconds) {//send pulses of width nPulseTimeMicroseconds for a duration of nDurationMilliseconds
+    unsigned int uiMoveEndTime = millis() + nDurationMilliseconds;
+    while (millis()<uiMoveEndTime) {
+        digitalWrite(DEPLOYMENT_PULSE_PIN,1);
+        usleep(nPulseTimeMicroseconds);
+        digitalWrite(DEPLOYMENT_PULSE_PIN,0);
+        unsigned int uiWaitTime = SD_PULSE_INTERVAL - nPulseTimeMicroseconds;
+        usleep(uiWaitTime);
+    }
+}
---
+++ SensorDeploy.h
@@ -0,0 +1,25 @@
+#define ZERO_SD_PULSE_TIME 1520 //pulse time in microseconds required for sending the midway angle to the servo motor connected to the sensor deployment arm
+#define MAX_SD_PULSE_TIME 2500 //maximum pulse time in microseconds, corresponds to the maximum possible servo motor angle, and the fully deployed state of the sensor deployment arm (i.e. sensors should be in the water)
+#define MIN_SD_PULSE_TIME 500 //mininum pulse time in microseconds, corresponds to the minimum possible servo motor angle, and the fully retracted state of the sensor deployment arm (i.e. sensors should be retracted out of the water onto the boat deck)
+#define SD_PULSE_INTERVAL 10000 //interval in microseconds between pulses
+#define DEPLOYMENT_PULSE_PIN 35 //pin used for sending pulses to the servo motor that is used for deploying / retracting sensors
+
+class SensorDeploy {
+public:
+	SensorDeploy(char *rootFolder, bool bInitializeWiringPi);//SensorDeploy constructor
+	~SensorDeploy();
+	void Deploy();//move the servo motor to deploy the sensors into the water
+	void Retract();//move the servo motor to retract the sensor from the water back onto the boat deck
+	static void OneTimeSetup();//setup procedure to be performed once per power-cycle of the servo deployment arm
+
+
+private:
+    //data
+    char *m_rootFolder;//the root program folder, where the prefs.txt preferences file is located (can be accessed to get last known sensor arm location)
+    bool m_bDeployed;//set to true when the sensor arm is fully deployed into the water
+    
+	//functions
+    void SaveSettings();//save the current state of the sensor deployment arm to the preferences file
+    void GetSettings();//try to get info about current state of sensor arm from preferences file
+    void Pulse (int nPulseTimeMicroseconds, int nDurationMilliseconds);//send pulses of width nPulseTimeMicroseconds for a duration of nDurationMilliseconds
+};
